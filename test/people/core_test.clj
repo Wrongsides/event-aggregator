55edd4bb0e5030cf4a9314380c6cbd397bfd69b0
John Byrne <johnbyrne.1984@gmail.com>
2019-08-03 23:04:45 +0100

    Fix people core tests

--- test/people/core_test.clj
+++ test/people/core_test.clj
@@ -1,7 +1,20 @@
 (ns people.core-test
-  (:require [clojure.test :refer :all]
+  (:require [cheshire.core :as json]
+            [clojure.test :refer :all]
             [people.core :refer :all]))
 
+(deftest test-name
+  (testing "Should shuffle names and return first value"
+    (let [names ["one","two"]
+          name (name names)]
+      (is (or (= "two" name) (= "one" name))))))
+
 (deftest test-generate-person
-  (testing "Should return a string person"
-    (is (= (generate-person) "person"))))
+  (testing "Should return person with name, age and gender"
+    (let [person (generate-person ["John"])]
+      (is (= "John" (:name person)))
+      (is (= [:name :gender :age] (keys person))))))
+
+(deftest test-produce-message
+  (testing "Should generate a person as json string"
+    (is (json/parse-string (produce-message ["John"])))))
